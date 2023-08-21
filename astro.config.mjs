28f04313fe859cd3b06e38020b64bbd7c05a7598
CodeSandbox <support@codesandbox.io>
2023-08-21 08:16:21 +0000

    Sandbox snapshot commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import image from "@astrojs/image";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), image()]
-});
+export default defineConfig({});
